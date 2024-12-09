3e9ed57869f5718da1c57e1f651897bf50f6c199
yglsan2 <le.thelonius@gmail.com>
2024-12-09 11:01:47 +0100

    Main + Adresse + Client 2 pb dans Client à améliorer

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
+import com.benja2.ProjetSociete1.Adresse;
+import com.benja2.ProjetSociete1.Prospect;
+import com.benja2.ProjetSociete1.Client;
 
 public class Main {
     public static void main(String[] args) {
 
+                Adresse adresse = new Adresse("123", "Rue des Lilas", "54000", "Nancy");
+                Client client = new Client(adresse, "Société ABC", "0123456789", "abc@exemple.com",
+                        "Client fidèle", 500000, 50);
 
-
+                client.afficher();
+            }
 
 
 
 
 
         }
-    }
+
+
--- src/com/benja2/ProjetSociete1/Adresse.java
+++ src/com/benja2/ProjetSociete1/Adresse.java
@@ -1,14 +1,56 @@
 package com.benja2.ProjetSociete1;
 
 public class Adresse {
-    private String adresse;
+    private String numeroRue;
+    private String nomRue;
+    private String codePostal;
+    private String ville;
 
-    public void setAdresse(String adresse) {
-        this.adresse = adresse;
+    // Constructeur
+    public Adresse(String numeroRue, String nomRue, String codePostal, String ville) {
+        this.numeroRue = numeroRue;
+        this.nomRue = nomRue;
+        this.codePostal = codePostal;
+        this.ville = ville;
     }
 
-    public String getAdresse() {
-        return adresse;
+    // Getters et Setters
+    public String getNumeroRue() {
+        return numeroRue;
+    }
 
+    public void setNumeroRue(String numeroRue) {
+        this.numeroRue = numeroRue;
+    }
+
+    public String getNomRue() {
+        return nomRue;
+    }
+
+    public void setNomRue(String nomRue) {
+        this.nomRue = nomRue;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    @Override
+    public String toString() {
+        return numeroRue + " " + nomRue + ", " + codePostal + " " + ville;
     }
 }
+
